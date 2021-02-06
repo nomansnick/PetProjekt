3ab089239b0b1c8f1841f25a012595ca5a2d621a
nomansnick <marapatgab@gmail.com>
2021-02-06 16:09:04 +0100

    typo fixes

--- src/QuestAndEnemy/MainQuest1.js
+++ src/QuestAndEnemy/MainQuest1.js
@@ -3,7 +3,7 @@ export const MQ1 = {
         BadText1 : "An old herbalist lives alone in the ",
         GoodText1 : "abandoned tower",
         BadText2 : " in the forest."
-        + "\n Some say he is just an eccentric healer, others believe he keeps sinister secrets."
+        + "\n Some say he is just an eccentric healer, others believe he has sinister secrets."
         + "\n Whatever the case, he does keep strange company."
         + "\n Every moon, ",
         GoodText2 : "a stanger clad in black",
@@ -14,7 +14,7 @@ export const MQ1 = {
         BadText1 : "A fortnight ago, two adventurers headed to the old Rustag Mine. "
         + "\n The long-abandoned mine is said to hold vast riches still, yet no one dares claim them. "
         + "\n The two, however, cast caution aside, and descended into the belly of the mountain through the mineshafts, "
-        + "\n daring the wrath of the dwellers of the deep. Alas, woe befell them! Four days passed, but only one of them emerged. "
+        + "\n daring the wrath of deep-dweller-monsters. Alas, woe befell them! Four days passed, but only one of them emerged. "
         + "\n Her face was grim, her look as ",
         GoodText1: "dark as the clothes she wore",
         BadText2: ". She held tightly onto ",
@@ -27,9 +27,9 @@ export const MQ1 = {
         GoodText1: "the slavers",
         BadText2: " of the resurgent Untari Empire. "
         + "\n And yet, there are those who deny the obvious. They say Untar has been beaten, her cities reduced to rubble. "
-        + "\n Their fear of war, their yearning for peace has made them soft, made the blind."
-        + "\n The truth, however, is there for those who are willing to see. The King's patrols have come upon Untari filth hiding in ",
+        + "\n Their fear of war, their yearning for peace has made them soft, made them blind."
+        + "\n The truth, however, is there for those willing to see. The King's patrols have come upon Untari filth hiding in ",
         GoodText2: "hidden fortifications.",
-        BadText3: "\n Whether they were slavers, spies, or cutthroats looking for easy pickings, avoid them like the plague!"
+        BadText3: "\n Whether slavers, spies, or cutthroats looking for easy pickings, avoid them like the plague!"
     }
 }
